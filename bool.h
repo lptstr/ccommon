e9eec452b924e6b4e63a41fadb6289b82644d5c9
Kiëd Llaentenn <kiedtl@protonmail.com>
2020-03-28 14:08:34 -0400

    bool.h: update

--- bool.h
+++ bool.h
@@ -1,8 +1,12 @@
 #ifndef CCOMMON_BOOL_H
 #define CCOMMON_BOOL_H
 
+#ifdef bool
+#undef bool
+#endif
+
 #define TRUE              1
 #define FALSE             0
-typedef _Bool          bool;
+#define bool          _Bool
 
 #endif
